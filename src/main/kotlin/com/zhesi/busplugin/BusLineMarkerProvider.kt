98b71936a4ab65b0b80f7578cf11e565a92b1fc4
lq <1464445232@qq.com>
2023-04-15 23:38:45 +0800

    实现java,kotlin中根据订阅查看对应事件发布者(接口方法)

--- src/main/kotlin/com/zhesi/busplugin/BusLineMarkerProvider.kt
+++ src/main/kotlin/com/zhesi/busplugin/BusLineMarkerProvider.kt
@@ -17,12 +17,22 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.PsiSuperMethodUtil
 import com.intellij.psi.util.PsiTreeUtil
 import com.zhesi.busplugin.common.Icons
-import org.jetbrains.kotlin.asJava.elements.KtLightField
 import org.jetbrains.kotlin.asJava.getRepresentativeLightMethod
+import org.jetbrains.kotlin.asJava.toLightClass
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.base.utils.fqname.fqName
+import org.jetbrains.kotlin.idea.base.utils.fqname.getKotlinFqName
+import org.jetbrains.kotlin.idea.caches.resolve.analyze
+import org.jetbrains.kotlin.idea.core.resolveType
 import org.jetbrains.kotlin.idea.stubindex.KotlinFullClassNameIndex
-import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
+import org.jetbrains.kotlin.nj2k.postProcessing.resolve
+import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.findFunctionByName
+import org.jetbrains.kotlin.psi.psiUtil.getReceiverExpression
+import org.jetbrains.kotlin.resolve.bindingContextUtil.getReferenceTargets
+import org.jetbrains.kotlin.resolve.calls.util.getType
+import org.jetbrains.kotlin.types.isError
 import javax.swing.Icon
 import kotlin.streams.toList
 
@@ -42,48 +52,128 @@ class BusLineMarkerProvider : RelatedItemLineMarkerProvider() {
         element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>
     ) {
 //        collectPandas(element)?.let { result.add(it) }
-        collectObservers(element)?.let { result.add(it) }
+//        collectObserversJava(element)?.let { result.add(it) }
+        collectObserversKt(element)?.let { result.add(it) }
     }
 
-    private fun collectObservers(element: PsiElement): RelatedItemLineMarkerInfo<PsiElement>? {
-        if (element.containingFile.name != "DataTableView.java") return null
-        if (element is PsiMethodCallExpression) {
-            if (!element.text.startsWith("EventBus")) return null
+    private fun collectObserversKt(element: PsiElement): RelatedItemLineMarkerInfo<PsiElement>? {
+        if (element.containingFile.name != "Workspace.kt") return null
+        if (element is KtCallElement) {
+            if (!element.text.startsWith("observe")) return null
 
             element.containingFile?.virtualFile ?: return null
             val project = element.project
 
-            // 找目标接口下的observe方法，获得来自的对象和订阅的事件，找到对象和发送事件(匹配所有子类)匹配的post调用
-            val tarMethod = element.resolveMethod() ?: return null
-            if (isBusObserveFun(project, tarMethod)) {
+            if (isBusObserveFun(project, element.getCallPsiMethod())) {
                 val callObj = element.getCallObj() ?: return null
                 val observeEventType = element.getObserveEventType() ?: return null
                 val targets = ArrayList<PsiElement>()
 
-                FilenameIndex.getAllFilesByExt(project, JavaFileType.DEFAULT_EXTENSION, GlobalSearchScope.projectScope(project))
-                    .mapNotNull { vf -> PsiManager.getInstance(project).findFile(vf) }
-                    .forEach {
-                        val tarObjPostCalls = PsiTreeUtil.findChildrenOfAnyType(it, PsiMethodCallExpression::class.java)
-                            .filter { call -> call.isBusPostFun() }
-                            .filter { call -> call.getCallObj() == callObj }
-                            .filter { call -> call.getPostEventType() == observeEventType }
-                            .toList()
-                        targets.addAll(tarObjPostCalls)
-                    }
+                addKtPostTargets(project, callObj, observeEventType, targets)
+                addJavaPostTargets(project, callObj, observeEventType, targets)
                 return createNavigationGutterIcon(element, targets)
             }
         }
         return null
     }
 
-    private fun PsiMethodCallExpression.getObserveEventType() =
+    private fun addKtPostTargets(
+        project: Project,
+        callObj: PsiField,
+        observeEventType: PsiElement,
+        targets: ArrayList<PsiElement>
+    ) {
+        FilenameIndex.getAllFilesByExt(project, KotlinFileType.EXTENSION, GlobalSearchScope.projectScope(project))
+            .mapNotNull { vf -> PsiManager.getInstance(project).findFile(vf) }
+            .forEach { psiFile ->
+                val tarObjPostCalls = PsiTreeUtil.findChildrenOfAnyType(psiFile, KtCallElement::class.java)
+                    .filter { call -> isBusPostFun(project, call.getCallPsiMethod()) }
+                    .filter { call -> call.getCallObj() == callObj }
+                    .filter { call -> call.getPostEventType()?.getKotlinFqName()?.let { it == observeEventType.getKotlinFqName() } ?: false }
+                    .toList()
+                targets.addAll(tarObjPostCalls)
+            }
+    }
+
+    private fun KtCallElement.getPostEventType(): KtClassOrObject? {
+        return valueArguments.firstOrNull()?.getArgumentExpression()?.resolveType()?.fqName?.asString()
+            ?.let { findKtClass(project, it) }
+    }
+
+    /**
+     * TODO: kotlin psi 无法完全解析 xxx.javaClass、xxx::class.java 的类型，只能通过字符串直接判断，当存在间接关系则无法处理
+     */
+    private fun KtCallElement.getObserveEventType(): KtClassOrObject? {
+        val rExp = valueArguments.firstOrNull()?.getArgumentExpression() ?: return null
+        val rType = rExp.let { it.getType(it.analyze()) }
+        if (rType?.isError == false) {
+            return rType.fqName?.asString()?.let { findKtClass(project, it) }
+        }
+        if (rExp.text.endsWith(".javaClass")) {
+            return ((rExp as? KtQualifiedExpression)?.receiverExpression as? KtReferenceExpression)?.resolve() as? KtClassOrObject
+        } else if (rExp.text.endsWith("::class.java")) {
+            return (((rExp as? KtQualifiedExpression)?.receiverExpression as? KtDoubleColonExpression)?.receiverExpression as? KtReferenceExpression)?.resolve() as? KtClassOrObject
+        }
+        return null
+    }
+
+    private fun KtCallElement.getCallFun() = (calleeExpression as? KtReferenceExpression)?.resolve() as? KtNamedFunction
+
+    private fun KtCallElement.getCallPsiMethod() = getCallFun()?.asPsiMethod()
+
+    private fun KtCallElement.getCallObj(): PsiField? {
+        val rRef = (calleeExpression as KtNameReferenceExpression).getReceiverExpression() as? KtNameReferenceExpression ?: return null
+        val rField: PsiField = when (val rPsi = rRef.getReferenceTargets(rRef.analyze()).singleOrNull()?.findPsi()) {
+            is KtObjectDeclaration -> rPsi.toLightClass()?.findFieldByName("INSTANCE", false) ?: return null
+            else -> return null
+        }
+        return rField
+    }
+
+    private fun collectObserversJava(element: PsiElement): RelatedItemLineMarkerInfo<PsiElement>? {
+        if (element is PsiMethodCallExpression) {
+            element.containingFile?.virtualFile ?: return null
+            val project = element.project
+
+            // 找目标接口下的observe方法，获得来自的对象和订阅的事件，找到对象和发送事件(匹配所有子类)匹配的post调用
+            if (element.isBusObserveFun()) {
+                val callObj = element.getCallObj() ?: return null
+                val observeEventType = element.getObserveEventType() ?: return null
+                val targets = ArrayList<PsiElement>()
+                addJavaPostTargets(project, callObj, observeEventType, targets)
+                addKtPostTargets(project, callObj, observeEventType, targets)
+                return createNavigationGutterIcon(element, targets)
+            }
+        }
+        return null
+    }
+
+    private fun addJavaPostTargets(
+        project: Project,
+        callObj: PsiField,
+        observeEventType: PsiElement,
+        targets: ArrayList<PsiElement>
+    ) {
+        FilenameIndex.getAllFilesByExt(project, JavaFileType.DEFAULT_EXTENSION, GlobalSearchScope.projectScope(project))
+            .mapNotNull { vf -> PsiManager.getInstance(project).findFile(vf) }
+            .forEach { psiFile ->
+                val tarObjPostCalls = PsiTreeUtil.findChildrenOfAnyType(psiFile, PsiMethodCallExpression::class.java)
+                    .filter { call -> call.isBusPostFun() }
+                    .filter { call -> call.getCallObj() == callObj }
+                    .filter { call -> call.getPostEventType()?.getKotlinFqName()?.let { it == observeEventType.getKotlinFqName() } ?: false }
+                    .toList()
+                targets.addAll(tarObjPostCalls)
+            }
+    }
+
+    private fun PsiMethodCallExpression.getObserveEventType(): PsiClass? =
         ((argumentList.expressions.firstOrNull()?.type as? PsiClassType)?.typeArguments()
             ?.firstOrNull() as? PsiClassReferenceType)?.resolve()
 
-    private fun PsiMethodCallExpression.getPostEventType() =
+    private fun PsiMethodCallExpression.getPostEventType(): PsiClass? =
         (argumentList.expressions.firstOrNull()?.type as? PsiClassType)?.resolve()
 
-    private fun PsiMethodCallExpression.getCallObj() = (methodExpression.qualifierExpression as? PsiReferenceExpression)?.resolve() as? KtLightField
+    private fun PsiMethodCallExpression.getCallObj() = (methodExpression.qualifierExpression as? PsiReferenceExpression)?.resolve() as? PsiField
 
     private fun getIBusClass(project: Project) = findKtClass(project, "com.nwpu.ucdp.util.IEventBus")
 
@@ -93,21 +183,23 @@ class BusLineMarkerProvider : RelatedItemLineMarkerProvider() {
 
     private fun PsiCall.isBusObserveFun() = resolveMethod()?.let { isBusObserveFun(project, it) } ?: false
 
-    private fun isBusPostFun(project: Project, tarMethod: PsiMethod): Boolean {
+    private fun isBusPostFun(project: Project, tarMethod: PsiMethod?): Boolean {
+        if (tarMethod == null) return false
         val iEventBusPostFun = getIBusFun(project, "post") ?: return false
-        return iEventBusPostFun.asPsiMethod()?.let { PsiSuperMethodUtil.isSuperMethod(tarMethod, it) } == true
+        return iEventBusPostFun.asPsiMethod()?.let { tarMethod == it || PsiSuperMethodUtil.isSuperMethod(tarMethod, it) } == true
     }
 
-    private fun isBusObserveFun(project: Project, tarMethod: PsiMethod): Boolean {
+    private fun isBusObserveFun(project: Project, tarMethod: PsiMethod?): Boolean {
+        if (tarMethod == null) return false
         val iEventBusObserveFun = getIBusFun(project, "observe") ?: return false
-        return iEventBusObserveFun.asPsiMethod()?.let { PsiSuperMethodUtil.isSuperMethod(tarMethod, it) } == true
+        return iEventBusObserveFun.asPsiMethod()?.let { tarMethod == it || PsiSuperMethodUtil.isSuperMethod(tarMethod, it) } == true
     }
 
     private fun KtFunction?.asPsiMethod() = this?.getRepresentativeLightMethod()
 
-    private fun findKtClass(project: Project, fqName: String): KtClass? {
+    private fun findKtClass(project: Project, fqName: String): KtClassOrObject? {
         val searchResult = KotlinFullClassNameIndex.getInstance().get(fqName, project, GlobalSearchScope.allScope(project))
-        return searchResult.firstOrNull() as? KtClass
+        return searchResult.singleOrNull()
     }
 
     private fun collectPandas(element: PsiElement): RelatedItemLineMarkerInfo<PsiElement>? {
